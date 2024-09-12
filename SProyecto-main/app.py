0490716744da9eb76541b6bf6e3931a7fd3cf92c
AngieG01 <angiegonzalez036@gmail.com>
2024-09-11 22:22:36 -0600

    se modificaron archivos y codigo en app

--- SProyecto-main/app.py
+++ SProyecto-main/app.py
@@ -4,8 +4,10 @@ import pymysql
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 
 pymysql.install_as_MySQLdb()
+app = Flask(__name__, static_folder='static')
+app.secret_key = 'ANGIESECRETA'
 
-app = Flask(__name__)
+#app = Flask(__name__)
 
 # Configuración de la base de datos
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost:3306/seminario'
@@ -13,7 +15,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # Modelo de la tabla de usuarios
-class Usuario(db.Model):
+class Usuarios(db.Model):
     _tablename_ = 'usuarios'
     id_usuario = db.Column(db.Integer, primary_key=True)
     usuario = db.Column(db.String(50), unique=True, nullable=False)
@@ -26,27 +28,28 @@ def login():
         username = request.form['usuario']
         password = request.form['contrasena']
 
-        user = Usuario.query.filter_by(usuario=username).first()
+        user = Usuarios.query.filter_by(usuario=username).first()
 
         if user and user.contrasena == password:
             session['user_id'] = user.id_usuario
             session['username'] = user.usuario
-            return redirect(url_for('layout'))
+            return redirect(url_for('home'))
         else:
             flash('Usuario o contraseña incorrectos')
 
     return render_template('Login.html')
 
-@app.route('/login')
-def login_page():
-    return render_template('Login.html')
-
 @app.route('/')
 def home():
-    return render_template('layout.html', tab='informacion')
+    if 'user_id' in session:
+        return render_template('layout.html', tab='informacion')
+    else:
+        return redirect(url_for('login'))
 
 @app.route('/pedidos')
 def tab1():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
     return render_template('layout.html', tab='pedidos')
 
 if __name__ == '__main__':
